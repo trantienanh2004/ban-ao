09865ed81a88c4b302f1173925eb250810c10be1
trantienanh2004 <trollkaka200@gmail.com>
2024-06-30 12:22:10 +0700

    capnhat

--- Du_An_Xuong/src/main/java/com/example/demo/entity/CoAo.java
+++ Du_An_Xuong/src/main/java/com/example/demo/entity/CoAo.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class CoAo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "nhập đi mà")
     @Column(name = "TEN_CO_AO")
     private String tencoao;
 
--- Du_An_Xuong/src/main/java/com/example/demo/service/ChatLieuService.java
+++ Du_An_Xuong/src/main/java/com/example/demo/service/ChatLieuService.java
@@ -3,6 +3,10 @@ package com.example.demo.service;
 import com.example.demo.entity.ChatLieu;
 import com.example.demo.repository.ChatLieuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +16,24 @@ public class ChatLieuService {
 @Autowired
     ChatLieuRepository chatLieuRepository;
 
+    int size = 3;
 
-public List<ChatLieu> ListChatlieu(){
+    public Page<ChatLieu> chatLieuPage (int pageNo){
+        Sort s = Sort.by(Sort.Direction.ASC,"id");
+        Pageable page = PageRequest.of(pageNo,size,s);
+        return chatLieuRepository.findAll(page);
+    }
+
+    public List<ChatLieu> ListChatlieu(){
     return chatLieuRepository.findAll();
+}
+public void xoaCL(int id){
+        chatLieuRepository.deleteById(id);
+}
+public void addCL(ChatLieu chatLieu){
+    chatLieuRepository.save(chatLieu);
+}
+public ChatLieu chatLieuByID(int id){
+       return chatLieuRepository.findById(id).orElse(null);
 }
 }
